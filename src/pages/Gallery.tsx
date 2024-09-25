871b270814d6b53854ee9fa2df23a582e312edfa
anibal andrade <anibal.campos@unplug.studio>
2024-09-25 09:49:49 -0600

    refactor: remove unused files and update TypeScript configuration for Vite

--- src/pages/Gallery.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactElement } from 'react'
-
-export default function GalleryPage(): ReactElement {
-	return (
-		<div className='m-2 grid min-h-screen grid-cols-[minmax(0,384px)] place-content-center gap-2 md:m-0 md:grid-cols-[repeat(2,minmax(0,384px))] xl:grid-cols-[repeat(3,384px)]'>
-			<h1> Pages/Gallery</h1>
-		</div>
-	)
-}
